749e201b6ff9665c12bb071430eb3bccb08f2240
mterrado <mcterrado@me.com>
2019-01-14 12:26:23 -0800

    Project setup in progress

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default props => (
+
+    <div>
+
+        <h1 className="center">Movie Quotes</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, reiciendis?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eos ipsam nulla quo ut vero?</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+
+    </div>
+
+)

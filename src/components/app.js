a9e695fc91cd52cc0a6afcde93deaaf1132287d5
mterrado <mcterrado@me.com>
2019-01-14 17:02:50 -0800

    All pages started, project setup complete

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,38 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 import React from 'react';
 import {Route} from 'react-router-dom';
+import About from './about';
 import Home from './home';
+import MovieList from './movie_list';
+import Nav from './nav';
+import SignIn from './sign_in';
+import SignUp from './sign_up';
+import MovieQuote from './movie_quote';
+
 
 
 const App = () => (
     <div>
+        <Nav/>
+
         <div className="container">
             <Route exact path="/" component={Home}/>
 
+            <Route path="/about" component={About}/>
+
+            <Route exact path="/movie-list" component={MovieList}/>
+
+            <Route path="/movie-list/:type" component={MovieList}/>
+
+            <Route path="/movie-quote" component={MovieQuote}/>
+
+            <Route path="/sign-in" component={SignIn}/>
+
+            <Route path="/sign-up" component={SignUp}/>
+
+
         </div>
+
     </div>
 );
 
---
+++ src/components/movie_quote.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class MovieQuote extends Component {
+    render(){
+        return (
+            <div className="center">
+                <h1>Movie Quote</h1>
+                <h5>Movie Quote: You ate me at hello.</h5>
+            </div>
+        );
+    }
+
+}
+
+export default MovieQuote;
